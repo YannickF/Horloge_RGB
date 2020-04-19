fc47ba749d5281f1c8c2f7fe90c6776bc81dfaae
Yannick F <y.fetre@laposte.net>
2020-04-20 01:24:52 +0200

    Create password.h

---
+++ password.h
@@ -0,0 +1,8 @@
+// mots de passe Wifi et mise à jour OTA
+// à personnaliser en fonction de votre réseau wifi et de vos paramètres OTA
+
+#ifndef STASSID
+  #define STASSID "SSID WIFI"
+  #define STAPSK  "WPA2 PASSWORD"
+#endif
+#define PASSWORD                "password"                  // mot de passe pour mise à jour OTA
